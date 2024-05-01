3ce4b30539684a96e906059e4c52501d33ce0265
Miguel <juanmigzmarq@gmail.com>
2024-05-01 11:53:09 +0700

    Added camera capabilities and being able to start and start camera

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,115 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use base64::{engine::general_purpose, Engine};
+use opencv::{
+    core::Vector,
+    imgcodecs::imencode,
+    prelude::*,
+    videoio::{self, VideoCapture},
+    Result,
+};
+use serde::Serialize;
+use std::sync::{Arc, Mutex};
+use tauri::{AppHandle, Manager, Window};
+
+#[derive(Clone, Serialize)]
+struct Payload {
+    message: String,
+}
+
+struct CameraOption {
+    camera_on: bool,
+}
+
+impl Default for CameraOption {
+    fn default() -> Self {
+        Self { camera_on: false }
+    }
+}
+
+// Camera
+fn camera_feed(cam: &mut VideoCapture) -> Result<String> {
+    let mut frame = Mat::default();
+    cam.read(&mut frame)?;
+
+    let params: Vector<i32> = Vector::new();
+    let mut buf: Vector<u8> = Vector::new();
+    imencode(".jpg", &frame, &mut buf, &params)?;
+
+    let b64 = general_purpose::STANDARD.encode(&buf);
+
+    Ok(format!("data:image/jpg;base64,{}", b64))
+}
+
+fn open_cam() -> Option<VideoCapture> {
+    let cam = videoio::VideoCapture::new(0, videoio::CAP_ANY);
+    match cam {
+        Ok(cam) => {
+            let opened = videoio::VideoCapture::is_opened(&cam).unwrap();
+            if !opened {
+                panic!("Camera is unavailable")
+            }
+
+            return Some(cam);
+        }
+
+        Err(_) => panic!("There is a problem opening OPENCV video capture"),
+    }
+}
+
+//commands
+
+#[tauri::command]
+async fn stop(app: AppHandle) -> Result<bool, String> {
+    let app_state = app.state::<Arc<Mutex<CameraOption>>>();
+    let arc_state = Arc::clone(&app_state);
+    let mut state = arc_state.lock().unwrap();
+    println!("Stopping camera");
+    state.camera_on = false;
+    Ok(state.camera_on.clone())
+}
+
+#[tauri::command]
+async fn start(app: AppHandle) -> Result<bool, String> {
+    let app_state = app.state::<Arc<Mutex<CameraOption>>>();
+    let arc_state = Arc::clone(&app_state);
+    let mut state = arc_state.lock().unwrap();
+    println!("Starting camera");
+    state.camera_on = true;
+    Ok(state.camera_on.clone())
+}
+
+#[tauri::command]
+async fn image_sending(window: Window, app: AppHandle) -> Result<(), String> {
+    let mut cam = open_cam().unwrap();
+    let app_state = app.state::<Arc<Mutex<CameraOption>>>();
+    let arc_state = Arc::clone(&app_state);
+    std::thread::spawn(move || loop {
+        std::thread::sleep(std::time::Duration::from_millis(1));
+        let state = arc_state.lock().unwrap();
+        if state.camera_on == true {
+            let feed = camera_feed(&mut cam);
+            match feed {
+                Ok(feed) => window
+                    .emit("image-byte", Payload { message: feed })
+                    .unwrap(),
+
+                Err(_) => panic!("There is a problem in the camera feed"),
+            }
+        } else {
+            cam.release().unwrap();
+            break;
+        }
+    });
+
+    Ok(())
+}
+
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .manage(Arc::new(Mutex::new(CameraOption::default())))
+        .invoke_handler(tauri::generate_handler![image_sending, stop, start])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,77 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import { invoke } from "@tauri-apps/api"
+import { listen } from "@tauri-apps/api/event"
+import { z } from "zod"
+
+interface Payload {
+	message: string
+}
+
+// const CAMERA_STATE = {
+// 	CAMERAON: "CAMERAON",
+// 	CAMERAOFF: "CAMERAOFF",
+// } as const
+
+// type CameraState = keyof typeof CAMERA_STATE
 
 function App() {
-	const [count, setCount] = useState(0)
+	const [image, setImage] = useState<string>("")
+	const [cameraState, setCameraState] = useState<boolean>(false)
 
-	return <div> Something just like this</div>
+	const handleStart = async () => {
+		invoke("start").then((message) =>
+			setCameraState(z.boolean().parse(message))
+		)
+	}
+
+	const handleStop = async () => {
+		invoke("stop").then((message) => setCameraState(z.boolean().parse(message)))
+	}
+
+	useEffect(() => {
+		if (cameraState == true) {
+			invoke("image_sending")
+		}
+	}, [cameraState])
+
+	useEffect(() => {
+		if (cameraState == true) {
+			const unlisten = listen("image-byte", (event) => {
+				let payload: Payload
+
+				payload = z.object({ message: z.string() }).parse(event.payload)
+				console.log(payload.message)
+				setImage(payload.message)
+			})
+			return () => {
+				unlisten.then((f) => f())
+			}
+		}
+	}, [cameraState])
+
+	return (
+		<div>
+			<h1 className="text-center mt-2">
+				Camera is: {cameraState ? "ON!" : "OFF!"}
+			</h1>
+			<div className="flex justify-center items-center">
+				{cameraState ? (
+					<img src={image} width="400" height="400" />
+				) : (
+					<div className=" bg-slate-500 w-[300px] h-[300px]"></div>
+				)}
+			</div>
+
+			<div className="flex justify-around items-center mt-4">
+				<button className=" bg-slate-500 text-white p-3" onClick={handleStart}>
+					start
+				</button>
+				<button className=" bg-slate-500 text-white p-3" onClick={handleStop}>
+					stop
+				</button>
+			</div>
+		</div>
+	)
 }
 
 export default App
